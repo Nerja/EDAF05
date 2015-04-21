3affa97d1b3da039effaad7c46017466699fb6df
JBarendt <jur12jba@student.lu.se>
2015-04-22 00:15:02 +0200

    speed up

--- labb3-julius/src/labb3/Prim.java
+++ labb3-julius/src/labb3/Prim.java
@@ -6,21 +6,16 @@ import java.util.List;
 public class Prim {
 
 	private List<Edge> m_possibleEdges;
-	private List<Node> m_visited;
-
 	private List<Edge> m_chosenEdges;
 
 	public Prim() {
-		m_visited = new LinkedList<Node>();
 		m_possibleEdges = new LinkedList<Edge>();
 		m_chosenEdges = new LinkedList<Edge>();
-
 	}
 
 	public int buildTree(List<Node> nodes) {
 		int result = 0;
 		Node startN = nodes.get(0);
-		m_visited.add(startN);
 		m_possibleEdges.addAll(startN.getEdges());
 
 		while (existPossibleMatch()) {
@@ -41,15 +36,9 @@ public class Prim {
 			cheapest.visit();
 			m_chosenEdges.add(cheapest);
 			result += cheapest.getWeight();
-			
-			if (!m_visited.contains(cheapest.getNodes()[0])) {
-				m_visited.add(cheapest.getNodes()[0]);
-				m_possibleEdges.addAll(cheapest.getNodes()[0].getEdges());
-			}
-			if (!m_visited.contains(cheapest.getNodes()[1])) {
-				m_visited.add(cheapest.getNodes()[1]);
-				m_possibleEdges.addAll(cheapest.getNodes()[1].getEdges());
-			}
+
+			m_possibleEdges.addAll(cheapest.getNodes()[0].getEdges());
+			m_possibleEdges.addAll(cheapest.getNodes()[1].getEdges());
 		}
 
 		return result;
